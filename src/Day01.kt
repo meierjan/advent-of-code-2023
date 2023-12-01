f9da52d801bdbf82413fa057b7e3e1d0880f76fe
Jan Meier <jan@meier.wtf>
2023-12-02 00:15:25 +0100

    Solves Day 1 - A & B

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,65 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val line = it
+            val firstNumber = line.first { it.isDigit() }.digitToInt()
+            val lastNumber = line.last { it.isDigit() }.digitToInt()
+            firstNumber * 10 + lastNumber
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = listOf(
+            "1",
+            "one",
+            "2",
+            "two",
+            "3",
+            "three",
+            "4",
+            "four",
+            "5",
+            "five",
+            "6",
+            "six",
+            "7",
+            "seven",
+            "8",
+            "eight",
+            "9",
+            "nine",
+        )
+
+        return input.sumOf { line ->
+            val firstNumber = numbers
+                .mapIndexed { index, it -> Pair(index, line.indexOf(it)) }
+                .filter { it.second != -1 }
+                .minBy { it.second }
+                .first / 2 + 1
+
+            val lastNumber = numbers.mapIndexed { index, it -> Pair(index, line.lastIndexOf(it)) }
+                .filter { it.second != -1 }
+                .maxBy { it.second }
+                .first / 2 + 1
+
+            firstNumber * 10 + lastNumber
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val result = part1(testInput)
+
+
+    check(result == 142)
+
+    val testInputB = readInput("Day01b_test")
+    val resultB = part2(testInputB)
+
+    check(resultB == 281)
+
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(input).println("Part 1:")
+    part2(input).println("Part 2:")
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,6 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+fun Any?.println(prefix: String) = kotlin.io.println("$prefix $this")
